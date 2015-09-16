d6c18234a76f9094e649c14bf27801b0a2bf3f37
Spockuto <venkythesupersaiyan@gmail.com>
2015-09-16 15:21:57 +0530

    Fixed Typo

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ read hour
 echo "Enter the minute 0-59(* for every)"
 read min
 crontab -r
-echo "$min $hour * * * ruby ${HOME}/ThePaperWall/Scripts/Wallpaper.rb" #>> tempfile
+echo "$min $hour * * * ruby ${HOME}/ThePaperWall/Scripts/Wallpaper.rb" >> tempfile
 crontab tempfile
 rm tempfile
 echo "Your Wallpaper will be updated at $hour:$min"

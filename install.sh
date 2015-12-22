9654b31d6aa54776733ee65d915c759da83c4164
Spockuto <venkythesupersaiyan@gmail.com>
2015-12-22 11:37:02 +0530

    Fixed crontab issue

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ mkdir -p ${HOME}/ThePaperWall/Scripts
 mkdir -p ${HOME}/ThePaperWall/Wallpapers
 ruby Wallpaper.rb
 cp Wallpaper.rb ${HOME}/ThePaperWall/Scripts/
+chmod -R 755 ${HOME}/ThePaperWall/Scripts/
 
 crontab -l > tempfile
-echo "30 16 * * * ruby ${HOME}/ThePaperWall/Scripts/Wallpaper.rb" >> tempfile
+echo "30 16 * * * /bin/bash -l -c 'ruby ${HOME}/ThePaperWall/Scripts/Wallpaper.rb'" >> tempfile
 crontab tempfile
 rm tempfile
 
